61dfaacfa6ede4d32570eb3545f349fd4bb948ad
chanel50 <106768673+Chanel50@users.noreply.github.com>
2023-09-14 14:35:38 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the <main> element with id 'main'
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Set the 'id' attribute of the new <h1> element to 'victory'
+newHeader.id = "victory";
+
+// Set the text content of the <h1> element (replace 'YOUR-NAME' with your actual name)
+newHeader.textContent = "YOUR-NAME is the champion"; // Replace 'YOUR-NAME' with your name
+
+// Append the <h1> element to the document body
+document.body.appendChild(newHeader);
